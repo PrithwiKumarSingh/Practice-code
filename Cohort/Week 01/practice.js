2655b38e6a588a5a88d1b807d5cbf5505469de7c
Prithwi Kumar <prithwikumar871@gmail.com>
2025-01-01 21:29:39 +0530

    slice method using

--- Cohort/Week 01/practice.js
+++ Cohort/Week 01/practice.js
@@ -1,10 +1,33 @@
 //length, lastIndexOf, indexOf
 
-function findIndexOf(str, target){
-    console.log("Original String:", str);
-    console.log("length:", str.length);
-    console.log(`index of ${str} : ${str.indexOf(target)}`)
-    console.log("indes:", str.lastIndexOf(target));
-}
-findIndexOf("Hello World World", "World");
+// function findIndexOf(str, target){
+//     console.log("Original String:", str);
+//     console.log("length:", str.length);
+//     console.log(`index of ${str} : ${str.indexOf(target)}`)
+//     console.log("indes:", str.lastIndexOf(target));
+// }
+// findIndexOf("Hello World World", "World");
 
+
+// slice
+
+function getSlice(str, start, end){
+    console.log("Original String:", str);
+    console.log("After slice:", str.slice(start, end));
+}
+getSlice("prithwi", 2,5);
+
+function cutIt(str, startIndex, endIndex){
+    let newStr = "";
+    for(let i=0; i<str.length; i++){
+        if(i >= startIndex && i<endIndex){
+            newStr = newStr + str[i]; 
+        }
+    }
+    return newStr;
+}
+
+const value = "harkirat singh";
+let ans2 = value.slice(2,5)
+console.log(ans2);
+console.log(cutIt(value, 2, 5));  
