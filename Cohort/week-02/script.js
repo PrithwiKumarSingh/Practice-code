80e23a8c198103574890a8556b5c290092e99bac
Prithwi Kumar <prithwikumar871@gmail.com>
2025-01-04 14:17:45 +0530

    Map using promises

--- Cohort/week-02/script.js
+++ Cohort/week-02/script.js
@@ -1,12 +1,41 @@
-// setTimout inside setTimeout 
+// // setTimout inside setTimeout 
 
-setTimeout(function (){
-    console.log("Hello World");
-    setTimeout(function (){
-        console.log("Hello Wolrd inside ");
-        setTimeout(function(){
-            console.log("Hello world inside of inside");
-        }, 8000)
-    },5000)
-}, 1000);
+// setTimeout(function (){
+//     console.log("Hello World");
+//     setTimeout(function (){
+//         console.log("Hello Wolrd inside ");
+//         setTimeout(function(){
+//             console.log("Hello world inside of inside");
+//         }, 8000)
+//     },5000)
+// }, 1000);
 
+//===============>>>>  map   <<<<=======================
+let num = [1, 2, 3, 4, 5];
+let double = num.map(function(el){
+    return el * 2; 
+})
+console.log(double);
+
+ 
+let squreValue = num.map(function(el){
+    return el * el;
+});
+
+console.log(squreValue);
+
+function eachElement(arr, num){
+    return new Promise(function(resolve){
+        resolve(arr.map(function(el){
+            return el * num;
+        }))
+    })
+}
+
+let arr = [1, 2, 3, 4, 5, 6];
+async function callEachElement() {
+    let value = await eachElement(arr, 4);
+    console.log(value);
+}
+
+callEachElement();
