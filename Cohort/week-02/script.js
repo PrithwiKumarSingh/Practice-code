1c196400a02d8a7a1e9799e0693147fc25f8bef3
Prithwi Kumar <prithwikumar871@gmail.com>
2025-01-04 14:35:47 +0530

    filter based practice

--- Cohort/week-02/script.js
+++ Cohort/week-02/script.js
@@ -11,31 +11,55 @@
 // }, 1000);
 
 //===============>>>>  map   <<<<=======================
-let num = [1, 2, 3, 4, 5];
-let double = num.map(function(el){
-    return el * 2; 
-})
-console.log(double);
+// let num = [1, 2, 3, 4, 5];
+// let double = num.map(function(el){
+//     return el * 2; 
+// })
+// console.log(double);
 
  
-let squreValue = num.map(function(el){
-    return el * el;
-});
+// let squreValue = num.map(function(el){
+//     return el * el;
+// });
 
-console.log(squreValue);
+// console.log(squreValue);
 
-function eachElement(arr, num){
-    return new Promise(function(resolve){
-        resolve(arr.map(function(el){
-            return el * num;
-        }))
-    })
+// function eachElement(arr, num){
+//     return new Promise(function(resolve){
+//         resolve(arr.map(function(el){
+//             return el * num;
+//         }))
+//     })
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// async function callEachElement() {
+//     let value = await eachElement(arr, 4);
+//     console.log(value);
+// }
+
+// callEachElement();
+
+
+//================>>>>>  Filter   <<<<==============
+
+//Question - Convert an array of numbers into a new array with only the even elements
+
+function generateArray(num){
+    let arr = [];
+    for(let i=0; i<num; i++){
+        arr.push(i);
+    }
+    return arr;
 }
 
-let arr = [1, 2, 3, 4, 5, 6];
-async function callEachElement() {
-    let value = await eachElement(arr, 4);
-    console.log(value);
-}
-
-callEachElement();
+// let newEven = arr.filter(function(el){
+//    return  el % 2 === 0;
+// })
+// using arrow fn 
+let n = 100;
+let newEvenfilter = generateArray(n).filter((num) => num % 2 == 0);
+// let newOddfilter = arr.filter((num) => num % 2 == 1);
+console.log("Original Array : ", generateArray(n));
+console.log("filtering Even : ",newEvenfilter);
+// console.log("filtering Odd : ", newOddfilter);
