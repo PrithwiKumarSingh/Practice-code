303fef89f7b31a9f803d5dc6935c6af542cb3e9c
Prithwi Kumar <prithwikumar871@gmail.com>
2025-01-06 00:46:15 +0530

    Hello world

--- Cohort/week-02/Http-server1/file.js
+++ Cohort/week-02/Http-server1/file.js
@@ -1,20 +1,22 @@
 const fs = require("fs");
 
-// Sync...(making file with value)
-fs.writeFileSync("./a.txt", "Hey there. kaise hai aap log " );
+// // Sync...(making file with value)
+// fs.writeFileSync("./a.txt", "Hey there. kaise hai aap log " );
 
-// Async .... (making file with value but  pass funciton )
-fs.writeFile("./a.txt", "prithwi singh", (err) => {});
+// // Async .... (making file with value but  pass funciton )
+// fs.writeFile("./a.txt", "prithwi singh", (err) => {});
 
-//sync
-const result = fs.readFileSync("./a.txt", "utf-8");
-console.log(result);
+// //sync
+// const result = fs.readFileSync("./a.txt", "utf-8");
+// console.log(result);
 
-//Async
-fs.readFile('./a.txt', 'utf-8', (err, result) => {
-    if(err){
-        console.log("Error : ",err);
-    }else{
-        console.log(result);
-    }
-});
+// //Async
+// fs.readFile('./a.txt', 'utf-8', (err, result) => {
+//     if(err){
+//         console.log("Error : ",err);
+//     }else{
+//         console.log(result);
+//     }
+// });
+
+fs.appendFileSync("./a.txt", `${Date.now()} Hey there\n`);
--- Cohort/week-02/Http-server1/index.js
+++ Cohort/week-02/Http-server1/index.js
@@ -1,4 +1,15 @@
-const math = require("./math")
-console.log(math);
+// const math = require("./math")
+// console.log(math);
 // console.log("Math Value is", math.sub(2, 4));
 // console.log("Math Value is", math.sum(2, 4));
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Prithwi Singh!')
+});
+
+app.listen(port);
