01dea6bd493574e9180c327e3734c186384f9526
Prithwi Kumar <prithwikumar871@gmail.com>
2025-01-05 14:25:18 +0530

    writeFile & readFile (sync/async)

---
+++ Cohort/week-02/Http-server1/file.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+// Sync...(making file with value)
+fs.writeFileSync("./a.txt", "Hey there. kaise hai aap log " );
+
+// Async .... (making file with value but  pass funciton )
+fs.writeFile("./a.txt", "prithwi singh", (err) => {});
+
+//sync
+const result = fs.readFileSync("./a.txt", "utf-8");
+console.log(result);
+
+//Async
+fs.readFile('./a.txt', 'utf-8', (err, result) => {
+    if(err){
+        console.log("Error : ",err);
+    }else{
+        console.log(result);
+    }
+});
