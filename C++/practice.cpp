e289fa43e112cd3e929aafdf2be26ae8e73809a9
Prithwi Kumar <coder-prithwi@prithwisingh.alphionsee.in>
2025-01-07 19:36:53 +0530

    Printing stars

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World";
+    int n;
+    cout<<"Please enter numbers : ";
+    cin>>n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
