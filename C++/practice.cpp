f6bdbec8ecbef33f042d50fd2633e866bf4b72d2
Prithwi Kumar <coder-prithwi@prithwisingh.alphionsee.in>
2025-01-08 23:05:43 +0530

    starting dsa journey Array

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,14 +1,24 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Please enter numbers : ";
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// =====================>>>> Array  <<<<======================
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"Please enter numbers : ";
-    cin>>n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout<<"* ";
-        }
-        cout<<endl;
+    int a[10];
+    for(int i=0; i<10; i++){
+        cin>>a[i];
     }
-    return 0;
 }
