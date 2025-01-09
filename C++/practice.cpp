e9ba74afe1adf735d4a2d720a172480f585e2798
Prithwi Kumar <coder-prithwi@prithwisingh.alphionsee.in>
2025-01-09 11:42:15 +0530

    find minimum value of arr

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -14,23 +14,40 @@
 // }
 
 // =====================>>>> Array  <<<<======================
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     // 1-method 
+//     // int a[5] = {3, 5, 54, 32, 5};
+//     // 2- method
+//     // int a[] = {3, 5, 45, 2, 65};
+//     // 3-method 
+//     // int a[5] = {0};
+//     //sizeof(int, float, char, bool)
+//     // float arr; 
+//     // cout<<sizeof(arr)<<" "<<endl;
+
+//     int a[5] = {3, 4, 5, 6, 9};
+//     cout<<sizeof(a)/sizeof(a[0])<<endl;
+//     // for(int i=0; i<5; i++)
+//     // cout<<a[i]<<" ";
+//     return 0;
+// }
+
+
+// find minimum value of this arr ?
 #include <iostream>
+#include <limits.h>
 using namespace std;
-
 int main(){
-    // 1-method 
-    // int a[5] = {3, 5, 54, 32, 5};
-    // 2- method
-    // int a[] = {3, 5, 45, 2, 65};
-    // 3-method 
-    // int a[5] = {0};
-    //sizeof(int, float, char, bool)
-    // float arr; 
-    // cout<<sizeof(arr)<<" "<<endl;
-
-    int a[5] = {3, 4, 5, 6, 9};
-    cout<<sizeof(a)/sizeof(a[0])<<endl;
-    // for(int i=0; i<5; i++)
-    // cout<<a[i]<<" ";
+    int arr[5] = {3, 5, 8, 9, 10};
+    int min = INT_MAX;
+    for(int i=0; i<5; i++){
+        if(arr[i]<min){
+            min = arr[i];
+        }
+    }
+    cout<<min<<endl;
     return 0;
 }
