1346cf02a569a9e8b5d62d3df75724eed43b95ec
Prithwi Kumar <coder-prithwi@prithwisingh.alphionsee.in>
2025-01-09 11:08:31 +0530

    array fundamentals

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -16,9 +16,21 @@
 // =====================>>>> Array  <<<<======================
 #include <iostream>
 using namespace std;
+
 int main(){
-    int a[10];
-    for(int i=0; i<10; i++){
-        cin>>a[i];
-    }
+    // 1-method 
+    // int a[5] = {3, 5, 54, 32, 5};
+    // 2- method
+    // int a[] = {3, 5, 45, 2, 65};
+    // 3-method 
+    // int a[5] = {0};
+    //sizeof(int, float, char, bool)
+    // float arr; 
+    // cout<<sizeof(arr)<<" "<<endl;
+
+    int a[5] = {3, 4, 5, 6, 9};
+    cout<<sizeof(a)/sizeof(a[0])<<endl;
+    // for(int i=0; i<5; i++)
+    // cout<<a[i]<<" ";
+    return 0;
 }
