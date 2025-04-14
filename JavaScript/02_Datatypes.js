a2b65b1b566935b85fddd197fa44cca9a828a181
Prithwi Kumar <prithwikumar871@gmail.com>
2025-04-14 13:14:12 +0530

    Datatype

---
+++ JavaScript/02_Datatypes.js
@@ -0,0 +1,20 @@
+let name = "Prithwi";
+let age = 21;
+let state = "Bihar";
+let City = "Motihari";
+
+let country;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null => standalone CSSMathValue
+// undefined =>
+// Symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined is undefined
+console.log(typeof null); // null is a object
+console.log(typeof age);
