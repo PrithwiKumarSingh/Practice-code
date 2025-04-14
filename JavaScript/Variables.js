057decf28f0852b4deee58ec8b74fc697e902df9
Prithwi Kumar <prithwikumar871@gmail.com>
2025-04-14 12:42:52 +0530

    Variables in Js

---
+++ JavaScript/Variables.js
@@ -0,0 +1,13 @@
+const accountId = 9880;
+let accountEmail = "prithwi@google.com";
+var accountPassword = "23432";
+accountCity = "Motihari";
+
+// accountId = 23;  //not allowed 
+
+accountEmail = "hc@gc.com";
+accountPassword = "34546";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
