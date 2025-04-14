d4d3114bfcaa3afc6484e1ae15a1f78b25663c5e
Prithwi Kumar <prithwikumar871@gmail.com>
2025-04-14 13:41:33 +0530

    Conversion

---
+++ JavaScript/03_Conversion.js
@@ -0,0 +1,21 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let valueInString = String(score);
+// console.log(typeof valueInString);
+// console.log(valueInString);
+
+let isLoggedIn = ""; 
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "prithwi" => true
+
