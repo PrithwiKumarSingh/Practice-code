d6235340b90ed48cc35292aa51dfbc30e80aa397
Prithwi Kumar <prithwikumar871@gmail.com>
2025-04-14 14:09:18 +0530

    operators

--- JavaScript/03_Conversion_Operators.js
+++ JavaScript/03_Conversion_Operators.js
@@ -13,9 +13,29 @@ let valueInString = String(score);
 
 let isLoggedIn = ""; 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "prithwi" => true
 
+
+// ****************** Operator ****************
+
+
+let str1 = "Prithwi";
+let str2 = " Singh";
+let str3 = str1 + str2;
+console.log(str3);
+
+let num1 = ("1" + 1);
+let num2 = (1 + "2");
+let num3 = ("1" + 1 + 2);
+let num4 = (1 + 2 + "3");
+
+console.table([num1, num2, num3, num4]);
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
