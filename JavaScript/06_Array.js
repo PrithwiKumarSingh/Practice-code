668cc83897af844b56a813fd3c22ec7c0e8e1544
Prithwi Kumar <prithwikumar871@gmail.com>
2025-04-28 21:14:37 +0530

    push,pop,shift & unshift

--- JavaScript/06_Array.js
+++ JavaScript/06_Array.js
@@ -6,18 +6,49 @@
 
 // Math 
 
-console.log(Math.E);
-console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.PI);
 
-console.log(Math.random());
+// console.log(Math.random());
 
-//floor & ceil 
-let num = 23.4;
-console.log(Math.floor(num));
-console.log(Math.ceil(num));
+// //floor & ceil 
+// let num = 23.4;
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
 
-console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
 
-//30-40
-console.log(Math.floor(Math.random)*10)+30();
-console.log(Math.floor(Math.random()*10)+1); 
+// //30-40
+// console.log(Math.floor(Math.random)*10)+30();
+// console.log(Math.floor(Math.random()*10)+1); 
+
+
+// const arr = [3, 4, 65, 23, 45, "Prithwi", true];
+// console.log(arr[5]); //Prithwi
+// console.log(arr[-1]); // undefined
+// console.log(arr.at(-1)); // true 
+// // at is latest, negative index le leta hai 
+// console.log(arr.length);
+
+
+const arr = [3, 4, 65, 23, 45, "Prithwi", true];
+
+// push() add element in last 
+
+arr.push(50);
+console.log(arr);
+
+// pop() delete element in last 
+
+arr.pop();
+console.log(arr);
+
+//unshift add element in start
+
+arr.unshift("Rohit");
+console.log(arr);
+
+// shift delete element in start
+
+arr.shift();
+console.log(arr);
