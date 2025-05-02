dcf5b59b8068995fd8e96048937d1afe285971bb
Prithwi Kumar <prithwikumar871@gmail.com>
2025-05-02 15:25:43 +0530

    Function

---
+++ JavaScript/09_Functions.js
@@ -0,0 +1,23 @@
+function greet(){
+    console.log("Hello welcome to my code. i hope you all are good.")
+
+}
+
+greet();
+//sum : function
+
+function sum( a,  b){
+    console.log(a+b);
+}
+
+sum(3,5);
+sum(34,5);
+
+// Multiplication : Function
+
+function multiply(a,b){
+    console.log(a*b);
+}
+
+multiply(3,6);
+multiply(54,3);
