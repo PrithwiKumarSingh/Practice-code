78851f908118de2a7e91bd79f328beadebbfac33
Prithwi Kumar <prithwikumar871@gmail.com>
2025-05-11 17:15:26 +0530

    Bgchange code optomize

--- JavaScript/19_projects_/calculator/BgChange/script.js
+++ JavaScript/19_projects_/calculator/BgChange/script.js
@@ -1,22 +1,30 @@
-const red = document.getElementById('red');
-const blue = document.getElementById('blue');
-const yellow = document.getElementById('yellow');
-const green = document.getElementById('green');
-const purple = document.getElementById('purple');
-const body = document.querySelector("body");
+// const red = document.getElementById('red');
+// const blue = document.getElementById('blue');
+// const yellow = document.getElementById('yellow');
+// const green = document.getElementById('green');
+// const purple = document.getElementById('purple');
+// const body = document.querySelector("body");
 
-red.addEventListener("click", ()=>{
-    body.style.backgroundColor = "red";
+// red.addEventListener("click", ()=>{
+//     body.style.backgroundColor = "red";
+// })
+// green.addEventListener("click", ()=>{
+//     body.style.backgroundColor = "green";
+// })
+// blue.addEventListener("click", ()=>{
+//     body.style.backgroundColor = "blue";
+// })
+// yellow.addEventListener("click", ()=>{
+//     body.style.backgroundColor = "yellow";
+// })
+// purple.addEventListener("click", ()=>{
+//     body.style.backgroundColor = "purple";
+// }) 
+
+const button = document.querySelectorAll("button");
+const body = document.querySelector("body");
+button.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        body.style.backgroundColor = button.cont;
+    })
 })
-green.addEventListener("click", ()=>{
-    body.style.backgroundColor = "green";
-})
-blue.addEventListener("click", ()=>{
-    body.style.backgroundColor = "blue";
-})
-yellow.addEventListener("click", ()=>{
-    body.style.backgroundColor = "yellow";
-})
-purple.addEventListener("click", ()=>{
-    body.style.backgroundColor = "purple";
-}) 
