ea543356bb0e79bff8d4a0126e36eed734204719
Prithwi Kumar <prithwikumar871@gmail.com>
2025-05-20 19:20:53 +0530

    Form (input)

--- JavaScript/19_projects_/calculator/Form/script.js
+++ JavaScript/19_projects_/calculator/Form/script.js
@@ -2,8 +2,13 @@ const form = document.querySelector('form');
 
 
 
-change
-form.addEventListener('change', (event) =>{
+// change
+// form.addEventListener('change', (event) =>{
+//     console.log(event.target.value);
+// })
+
+input
+form.addEventListener('input', (event) =>{
     console.log(event.target.value);
 })
 
