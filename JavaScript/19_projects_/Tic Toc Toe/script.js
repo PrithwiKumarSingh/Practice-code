b025f35c7680490de828414b5a135f5561e19497
Prithwi Kumar <prithwikumar871@gmail.com>
2025-06-01 19:33:07 +0530

    completed Tic Toc Toe Game

--- JavaScript/19_projects_/Tic Toc Toe/script.js
+++ JavaScript/19_projects_/Tic Toc Toe/script.js
@@ -31,29 +31,46 @@ const printer = (event) => {
         total_turn++;
     if(turn==="O")
         { 
+        ch2.style.scale = "1";
+        const ch1 = document.getElementById('ch1');
+        ch1.style.scale = "1.2";
         elem.innerHTML = "O";
+        elem.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px red, 0 0 30px red, 0 0 40px red, 0 0 55px red, 0 0 70px red;"
         board_array[elem.id] = "O"
         if(checkWinner()){
-            document.getElementById('winningMassage').innerHTML = "Winner is O";
+            document.getElementById('pl1').innerHTML = " !! Winner !!";
+            document.getElementById('pl1').style.color = "greenyellow";
+            document.getElementById('pl2').innerHTML= "! Losser !";
+            document.getElementById('pl2').style.color = "#800000";
+
             board.removeEventListener("click",printer);
             return;
         }
         turn = "X";
+
     } 
     else{
+        ch1.style.scale = "1";
+        const ch2 = document.getElementById('ch2');
+        ch2.style.scale = "1.2";
         elem.innerHTML = "X";
+        elem.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 15px #0073e6, 0 0 20px #0073e6, 0 0 25px #0073e6, 0 0 30px #0073e6, 0 0 35px #0073e6"
         board_array[elem.id] = "X"
         if(checkWinner()){
-            document.getElementById('winningMassage').innerHTML = "Winner is X";
+            document.getElementById('pl2').innerHTML = " !! Winner !!";
+            document.getElementById('pl2').style.color = "greenyellow";
+            document.getElementById('pl1').innerHTML= "! Losser !";
+            document.getElementById('pl1').style.color = "#800000";
             board.removeEventListener("click",printer);
             return;
         }
         turn = "O";
+
     }
 }
 if(total_turn==9){
     document.getElementById('winningMassage').innerHTML = "Match is Draw !";
-    document.getElementById('winningMassage').style.color = "red";
+    document.getElementById('winningMassage').style.color = "white";
     board.removeEventListener("click",printer);
     
 }
@@ -76,5 +93,9 @@ Restart.addEventListener('click', ()=>{
     board_array = new Array(9).fill("E");
     document.getElementById('winningMassage').innerHTML = "";
     board.addEventListener('click', printer);
+    document.getElementById('ch1').style.scale = '1';
+    document.getElementById('ch2').style.scale = '1';
+    document.getElementById('pl1').innerHTML = "";
+    document.getElementById('pl2').innerHTML = "";
 
 })
