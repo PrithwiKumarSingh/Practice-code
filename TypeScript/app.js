fbd027d3f3a92dc8f004e49c172fe0904e43f061
Prithwi Kumar <prithwikumar871@gmail.com>
2025-06-11 20:58:57 +0530

    Basics of TypeScript

---
+++ TypeScript/app.js
@@ -0,0 +1,3 @@
+"use strict";
+let a = 34;
+let b = 32;
---
+++ TypeScript/app.ts
@@ -0,0 +1,23 @@
+// Number
+
+let a:number = 34;
+let b:number = 32;
+
+// String
+
+let str:string = "Prithwi";
+
+// boolean
+let isExist:boolean = true;
+isExist = false;
+
+//bigint
+
+// let bignum: bigint = 34242343453534534534534;
+
+
+//null
+
+let abc:null = null;
+let bcd:undefined = undefined;
+// bcd = "Mohan"
